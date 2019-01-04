12ba830904d5641df162086f79fa17dfa3e61e31
okccc <1573976179@qq.com>
2019-01-04 18:06:26 +0800

    使用bs4解析html文档不用去除标签内的子标签就可用获取完整text

--- db/mongodb/demo.py
+++ db/mongodb/demo.py
@@ -1,15 +1,35 @@
 # coding=utf-8
 import pymongo
+import csv
+import codecs
 
 
 def test():
-    # 参考文档 https://www.jianshu.com/p/c9777b063593
+    # 创建数据库连接
     conn = pymongo.MongoClient("mongodb://root:lzkhxf3xtewUlctyazH5@10.9.78.195:27017/", connect=False)
-    db = conn["stat_dev"]
-    collection = db["debitLoanV2"]
-    with open("C://Users/chenqian/Desktop/result.json", "w") as f:
-        for i in collection.find():
-            f.write(str(i) + "\n")
+    # 库
+    db = conn.stat_dev
+    # 集合
+    collection = db.debitLoanV2
+    # 文档查询
+    documents = []
+    # 将不需要的字段设为0
+    for document in collection.find({}, {"_id": 0, "_class": 0}):
+        print(document)
+        documents.append(document)
+    print(documents)
+    # 按照生成的字段顺序生成表头
+    header = list(documents[0].keys())
+    # with open("C://Users/chenqian/Desktop/result.csv", "wb") as file:
+    #     file.write(codecs.BOM_UTF8)
+
+    with open("C://Users/chenqian/Desktop/aaa.csv", "w", encoding="utf8", newline="") as file:
+        # 创建writer对象
+        writer = csv.DictWriter(file, fieldnames=header)
+        # 先写入一行表头
+        writer.writeheader()
+        # 再写入多行数据
+        writer.writerows(documents)
 
 
 if __name__ == '__main__':
