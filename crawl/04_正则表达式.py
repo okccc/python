2623e553e8cb922b0c47039a1c010d3467a5ade6
okccc <1573976179@qq.com>
2019-03-01 11:04:50 +0800

    python正则表达式

--- crawl/04_正则表达式.py
+++ crawl/04_正则表达式.py
@@ -34,9 +34,10 @@ $ : 字符串结尾
 
 import re
 import urllib.request
+import chardet
 
-def regex():
-    # pattern = re.compile("")：编译正则表达式生成pattern对象,供match()、search()使用
+def regex01():
+    # pattern = re.compile(r"...")：编译正则表达式生成pattern对象,供match()、search()、findall()匹配字符串
 
     # 1.match()：只匹配字符串的开始,返回第一个符合的字符串,不符合就返回None
     pattern = re.compile("\d+")
@@ -48,7 +49,7 @@ def regex():
     print(m)  # <_sre.SRE_Match object; span=(3, 5), match='12'>
     print(m.group())  # 12
 
-    pattern = re.compile("([a-z]+) ([a-z]+)", re.I)  # re.I表示忽略大小写;re.S表示全文匹配
+    pattern = re.compile("([a-z]+) ([a-z]+)", re.I)  # re.I表示忽略大小写,re.S表示全文匹配
     m = pattern.match("Hello World Hello python")
     # group()方法返回符合规则的组,不写默认0
     print(m.group(0))  # Hello World
@@ -75,12 +76,12 @@ def regex():
     f = pattern.findall("abc 123 def 456")
     # findall()返回的是列表,不需要调用group()
     print(f)  # ['123', '456']
-    pattern = re.compile("\d*")  # *表示贪婪模式: 尽可能获取多的
+    pattern = re.compile("\d+.*")  # *表示贪婪模式(默认)
     f = pattern.findall("abc 123 def 456")
-    print(f)  # ['', '', '', '', '123', '', '', '', '', '', '456', '']
-    pattern = re.compile(r"\d?")  # ?表示非贪婪模式: 尽可能获取少的
+    print(f)  # ['123 def 456']
+    pattern = re.compile("\d+?")  # ?表示非贪婪模式
     f = pattern.findall("abc 123 def 456")
-    print(f)  # ['', '', '', '', '1', '2', '3', '', '', '', '', '', '4', '5', '6', '']
+    print(f)  # ['1', '2', '3', '4', '5', '6']
 
     # 4.split()：切割,返回列表
     s = re.split("[\d\s\;]+", "abc12 de;34\fg")
@@ -94,77 +95,33 @@ def regex():
     s2 = re.sub("\s", "", str2)
     print(s2)  # hellopython
 
-class NeihanduanziSpider(object):
-    """
-    用正则表达式爬内涵段子
-    """
 
-    # 初始化
-    def __init__(self):
-        self.page = 1
-        self.switch = True
-
-    # 爬取数据
-    def loadPage(self):
-
-        # 待爬取url
-        url = "http://www.neihanpa.com/article/list_5_" + str(self.page) + ".html"
-        # http请求头
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36"}
-        # 创建请求对象
-        request = urllib.request.Request(url, headers=headers)
-
-        # 使用代理IP
-        # proxy_list = [
-        #     {"http": "113.89.54.209:9999"},
-        #     {"http": "61.50.244.179:808"},
-        #     {"http": "58.220.95.107:8080"}
-        # ]
-        # proxy = random.choice(proxy_list)
-        # proxy_handler = urllib.request.ProxyHandler(proxy)
-        # opener = urllib.request.build_opener(proxy_handler)
-        # response = opener.open(request)
-
-        # 发送请求
-        response = urllib.request.urlopen(request)
-
-        # chardet可以测试网页编码
-        # res = chardet.detect(response.read())
-        # print(res)  # {'language': 'Chinese', 'confidence': 0.99, 'encoding': 'GB2312'}
-
-        # 爬网页的时候要注意网页源码的charset,乱码时要用decode()做解码
-        # ignore参数表示忽略非gb2312编码的字符,因为网站可能会注入少量其它字符
-        html = response.read().decode("gb2312", "ignore")
-        print(type(html))  # <class 'str'>
-        # print(html)
-
-        # 使用正则过滤数据
-        pattern = re.compile('<div class="f18 mb20">(.*?)</div>', re.S)
-        result = pattern.findall(html)
-        print(type(result))  # <class 'list'>
-        # print(result)
-        print("*****正在爬取第 %d 页数据*****" % self.page)
-        for item in result:
-            item = item.replace("<p>", "").replace("</p>", "").replace("<br />", "")
-            self.writeToFile(item)
-
-    # 保存到本地
-    def writeToFile(self, item):
-        f = open("D://data.txt", "a", encoding="utf-8")
-        f.write(item)
-        f.write("=" * 100)
-        f.close()
-
-    # 循环爬取
-    def work(self):
-        while self.switch:
-            self.loadPage()
-            self.page += 1
+# 爬取数据
+def regex02():
+    # 请求地址
+    url = "http://www.neihanpa.com/article/list_5_1.html"
+    # http请求头
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36"}
+    # 创建请求对象
+    request = urllib.request.Request(url, headers=headers)
+    # 发送请求
+    response = urllib.request.urlopen(request)
+    # chardet测试网页编码
+    res = chardet.detect(response.read())
+    print(res)  # {'language': 'Chinese', 'confidence': 0.99, 'encoding': 'GB2312'}
+    # 爬网页的时候要注意网页源码的charset,乱码时要用decode()做解码;ignore参数表示忽略非gb2312编码的字符,因为网站可能会注入少量其它字符
+    html = response.read().decode("gb2312", "ignore")
+    print(type(html))  # <class 'str'>
+    # 使用正则过滤数据
+    pattern = re.compile('<div class="f18 mb20">(.*?)</div>', re.S)
+    items = pattern.findall(html)
+    print(type(items))  # <class 'list'>
+    for item in items:
+        item = item.replace("<p>", "").replace("</p>", "").replace("<br />", "")
+        with open("D://data.txt", "a", encoding="utf-8") as f:
+            f.write(item)
 
 
 if __name__ == "__main__":
-    regex()
-
-    # s = NeihanduanziSpider()
-    # s.work()
+    # regex01()
+    regex02()
