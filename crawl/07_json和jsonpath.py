6b853c2f89ea32790abe39b5a3d698390a5be34d
okccc <1573976179@qq.com>
2019-03-01 16:39:39 +0800

    json和jsonpath

--- crawl/07_json和jsonpath.py
+++ crawl/07_json和jsonpath.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 """
 JSON(JavaScript Object Notation):
-1、JavaScript对象表示法,独立于语言和平台
-2、轻量级的文本数据交换格式(比如网站前后台的数据交换)
-3、类似xml且比xml更小更快更易解析
-json简单说就是javascript中的对象和数组,通过这两种结构可以表示各种复杂的结构
-对象: 在js中用{}表示,数据结构为{key: value}键值对,通过key取值;key是字符串,value可以是字符串、数字、数组、对象等
+1.JavaScript对象表示法,独立于语言和平台
+2.轻量级的文本数据交换格式(比如网站前后台的数据交换)
+3.类似xml且比xml更小更快更易解析
+json其实就是javascript中的数组和对象,通过这两种结构可以表示各种复杂的结构
 数组: 在js中用[]表示,数据结构为["python", "java"],通过索引取值;字段值类型可以是字符串、数字、数组、对象等
+对象: 在js中用{}表示,数据结构为{key: value}键值对,通过key取值;key是字符串,value可以是字符串、数字、数组、对象等
 json模块提供了四个功能: dumps、dump、loads、load,用于Json字符串和Python对象之间的序列化/反序列化
 
 JsonPath: 遍历Json对象中的节点;JsonPath之于Json相当于XPath之于XML
@@ -30,42 +30,47 @@ import jsonpath
 import chardet
 import requests
 
-"""
-json.dumps(obj): 将Python对象序列化成Json格式的字符串(Serialize obj to a JSON formatted str)
-序列化: 把变量从内存中变成可存储或传输的过程,延长生命周期
-注意: json.dumps()序列化默认使用ascii编码,处理中文时要添加参数ensure_ascii=False禁用ascii编码,按utf-8编码
-chardet.detect(byte_str)可以测试网页编码,参数类型必须是bytes or bytearray
-"""
 def dumps():
-    tuple = (1, 2, 3, 4)
-    list = [1, 2, 3, 4]
-    dict = {"city": "上海", "name": "grubby"}
-    print(json.dumps(tuple))  # [1, 2, 3, 4]
-    print(type(json.dumps(tuple)))  # <class 'str'>
-    print(json.dumps(list))  # [1, 2, 3, 4]
-    print(type(json.dumps(list)))  # <class 'str'>
-
-    print(json.dumps(dict))  # {"city": "\u4e0a\u6d77", "name": "grubby"}
-    print(type(json.dumps(dict)))  # <class 'str'>
+    """
+    json.dumps(obj)：将Python对象序列化成Json格式的字符串(Serialize obj to a JSON formatted str)
+    序列化：把变量从内存中变成可存储或传输的过程,延长生命周期
+    注意：json.dumps()序列化默认使用ascii编码,处理中文时要添加参数ensure_ascii=False禁用ascii编码,按utf-8编码
+    chardet.detect(byte_str)可以测试网页编码,参数类型必须是bytes or bytearray
+    """
+    T = (1, 2, 3, 4)
+    L = [1, 2, 3, 4]
+    D = {"city": "上海", "name": "grubby"}
+    print(json.dumps(T))  # [1, 2, 3, 4]
+    print(type(json.dumps(T)))  # <class 'str'>
+    print(json.dumps(L))  # [1, 2, 3, 4]
+    print(type(json.dumps(L)))  # <class 'str'>
+    print(json.dumps(D))  # {"city": "\u4e0a\u6d77", "name": "grubby"}
+    print(type(json.dumps(D)))  # <class 'str'>
 
     # 将str编码成bytes
-    res = json.dumps(dict).encode('utf-8')
+    res = json.dumps(D).encode('utf-8')
     print(type(res))  # <class 'bytes'>
     print(chardet.detect(res))  # {'encoding': 'ascii', 'confidence': 1.0, 'language': ''}
 
-    print(json.dumps(dict, ensure_ascii=False))  # {"name": "grubby", "city": "上海"}
+    print(json.dumps(D, ensure_ascii=False))  # {"name": "grubby", "city": "上海"}
     # 将str编码成bytes
-    res = json.dumps(dict, ensure_ascii=False).encode('utf-8')
+    res = json.dumps(D, ensure_ascii=False).encode('utf-8')
     print(type(res))  # <class 'bytes'>
     print(chardet.detect(res))  # {'encoding': 'utf-8', 'language': '', 'confidence': 0.7525}
 
+def dump():
+    """
+    json.dump()：将Python对象序列化成json字符串后写入文件(Serialize obj as a JSON formatted stream to fp (a .write()-supporting file-like object).)
+    """
+    L = [{"city": "上海"}, {"name": "grubby"}]
+    D = {"city": "上海", "name": "grubby"}
+    json.dump(L, open("C://Users/Public/list.json", "w", encoding="utf-8"), ensure_ascii=False)
+    json.dump(D, open("C://Users/Public/dict.json", "w", encoding="utf-8"), ensure_ascii=False)
 
-"""
-json.loads(): 将包含Json文档的字符串实例反序列化成Python对象
-(Deserialize s (a str instance containing a JSON document) to a python object)
-注意: a str是一个整块,不能是多个小的块(loads/load does not decode multiple json object)
-"""
 def loads():
+    """
+    json.loads()：将包含Json文档的字符串实例反序列化成Python对象(Deserialize s (a str instance containing a JSON document) to a python object)
+    """
     list_str = '[1, 2, 3, 4]'
     dict_str = '{"city": "上海", "name": "grubby"}'
     print(json.loads(list_str))  # [1, 2, 3, 4]
@@ -73,23 +78,10 @@ def loads():
     print(json.loads(dict_str))  # {'name': 'grubby', 'city': '上海'}
     print(type(json.loads(dict_str)))  # <class 'dict'>
 
-
-"""
-json.dump(): 将Python对象序列化成json字符串后写入文件
-(Serialize obj as a JSON formatted stream to fp (a .write()-supporting file-like object).)
-"""
-def dump():
-    list = [{"city": "上海"}, {"name": "grubby"}]
-    dict = {"city": "上海", "name": "grubby"}
-    json.dump(list, open("C://Users/Public/list.json", "w", encoding="utf-8"), ensure_ascii=False)
-    json.dump(dict, open("C://Users/Public/dict.json", "w", encoding="utf-8"), ensure_ascii=False)
-
-
-"""
-json.load(): 将文件中json字符串反序列化成python对象
-(Deserialize fp (a .read()-supporting file-like object containing a JSON document) to a python object)
-"""
 def load():
+    """
+    json.load()：将文件中json字符串反序列化成python对象(Deserialize fp (a .read()-supporting file-like object containing a JSON document) to a python object)
+    """
     list_str = json.load(open("C://Users/Public/list.json", encoding="utf-8"))
     dict_str = json.load(open("C://Users/Public/dict.json", encoding="utf-8"))
     print(list_str)  # [{'city': '上海'}, {'name': 'grubby'}]
@@ -98,42 +90,40 @@ def load():
     print(type(dict_str))  # <class 'dict'>
 
 
-"""
-需求: 获取拉勾网城市Json文件http://www.lagou.com/lbs/getAllCitySearchLabels.json的所有城市
-jsonpath.jsonpath(obj, express): traverse JSON object using jsonpath expr, returning values or paths
-"""
 def lagou():
+    """
+    需求: 获取拉勾网城市Json文件http://www.lagou.com/lbs/getAllCitySearchLabels.json的所有城市
+    jsonpath.jsonpath(obj, express): traverse JSON object using jsonpath expr, returning values or paths
+    """
+
     # 文件链接
     url = "http://www.lagou.com/lbs/getAllCitySearchLabels.json"
     # 请求头
     headers = {"User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)"}
     # 发送get请求
     response = requests.get(url, headers=headers)
-    # 获取数据
-    text = response.text
-    print(type(text))  # <class 'str'>
     # 将json格式的字符串转换成Python对象
-    dict = json.loads(text)
-    print(type(dict))  # <class 'dict'>
+    D = json.loads(response.text)
+    print(type(D))  # <class 'dict'>
     # 通过JsonPath表达式解析
-    city_list = jsonpath.jsonpath(dict, '$..name')
-    print(type(city_list))  # <class 'list'>
+    citys = jsonpath.jsonpath(D, '$..name')
+    print(type(citys))  # <class 'list'>
     # for city in city_list:
     #     print(city)
 
     # 将list序列化成json数组
-    array = json.dumps(city_list, ensure_ascii=False)
-    # print(type(array))  # <class 'str'>
-    # print(chardet.detect(array.encode('utf-8')))  # {'encoding': 'utf-8', 'confidence': 0.99, 'language': ''}
+    array = json.dumps(citys, ensure_ascii=False)
+    print(type(array))  # <class 'str'>
+    print(chardet.detect(array.encode('utf-8')))  # {'encoding': 'utf-8', 'confidence': 0.99, 'language': ''}
 
     # 写入本地文件
-    with open("C://Users/qmtv/Documents/city.txt", "w") as f:
+    with open("D://city.txt", "w") as f:
         f.write(array)
 
 
 if __name__ == "__main__":
-    dumps()
+    # dumps()
     # loads()
     # dump()
     # load()
-    # lagou()
+    lagou()
