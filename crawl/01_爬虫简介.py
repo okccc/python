1b26015dc539336597ef2c829e26feb434d86184
okccc <1573976179@qq.com>
2019-03-11 18:05:35 +0800

    http请求头

--- crawl/01_爬虫简介.py
+++ crawl/01_爬虫简介.py
@@ -13,26 +13,25 @@ DNS：将域名映射成IP地址的域名解析服务
 
 http请求方式
 get是从服务器上获取数据,post是向服务器传送数据
-get请求：参数显示在浏览器的url中,例如：http://www.baidu.com/s?wd=Chinese
-post请求：参数在请求体当中,以隐式的方式发送,请求的参数包含在"Content-Type"消息头里,指明该消息体的媒体类型和编码
-注意：尽量避免get方式提交表单,可能会导致安全问题
+get请求：参数显式的写在浏览器的url中,例如：http://www.baidu.com/s?wd=Chinese
+post请求：参数在请求体当中,以隐式的方式发送,请求的参数包含在"Content-Type"消息头里,指明该消息体的媒体类型和编码 ---> 表单提交、大文本传输
 
 http请求包含：请求行,请求头,空行,请求体(post)
 POST http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule HTTP/1.1
-Host：fanyi.youdao.com
-Connection：keep-alive
-Content-Length：202
-Accept：application/json, text/javascript, */*; q=0.01
-Origin：http://fanyi.youdao.com
-X-Requested-With：XMLHttpRequest
-User-Agent：Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36
+Host：fanyi.youdao.com                                           ---> 主机和端口号
+Connection：keep-alive                                           ---> 连接类型
+Upgrade-Insecure-Requests                                        ---> 升级为HTTPS请求
+Accept：application/json, text/javascript, */*; q=0.01           ---> 传输文件类型
+User-Agent：...                                                  ---> 浏览器名称(重要)
 Content-Type：application/x-www-form-urlencoded; charset=UTF-8
-Referer：http://fanyi.youdao.com/
-Accept-Encoding：gzip, deflate
-Accept-Language：zh-CN,zh;q=0.9
-Cookie：OUTFOX_SEARCH_USER_ID=-388253338@10.169.0.84; JSESSIONID=aaal-CPvBwa9P85-5f7ew; OUTFOX_SEARCH_USER_ID_NCOO=2060469077.47785; fanyi-ad-id=39535; fanyi-ad-closed=1; ___rl__test__cookies=1517115259862
+Origin：http://fanyi.youdao.com
+Referer：http://fanyi.youdao.com/                                ---> 页面跳转处
+Accept-Encoding：gzip, deflate                                   ---> 文件编解码格式
+Accept-Language：zh-CN,zh;q=0.9                                  ---> 浏览器愿意接收的数据 q表示权重
+Cookie：...                                                      ---> Cookie(重要)
+X-Requested-With：XMLHttpRequest                                 ---> 说明是ajax异步请求
 
-i=rabbit&from=AUTO&to=AUTO&smartresult=dict&client=fanyideskweb&salt=1517115259867&sign=cb455e27816176d6dbdbd09cfa87c8cd&doctype=json&version=2.1&keyfrom=fanyi.web&action=FY_BY_REALTIME&typoResult=false
+i=rabbit&from=AUTO&to=AUTO&smartresult=dict&client=fanyideskweb
 
 http状态码汇总
 200：请求成功
