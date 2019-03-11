84a9f79dd560d75f8d6eb6777d4b12bcb3e81fdc
okccc <1573976179@qq.com>
2019-03-11 17:02:53 +0800

    二叉树深度优先遍历和广度优先遍历

--- dsa/06_二叉树.py
+++ dsa/06_二叉树.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 """
-树：是一种抽象数据类型,用来模拟具有树状结构的数据集合
+树(二维)：是一种抽象数据类型,用来模拟具有树状结构的数据集合
 
 常用术语：
-节点的度：节点含有的子树个数
+节点的度：节点的子树个数
 树的度：最大的节点的度就是树的度
 叶节点：度为0的节点
 节点的层次：根为第一层,下面是第二层,依次类推
-树的深度：树种节点的最大层次
+树的深度：树中节点的最大层次
 
 二叉树：每个节点最多含有两个子节点的树称为二叉树
 完全二叉树：除了最底层,其他各层节点数均达到最大值
@@ -17,8 +17,7 @@
 
 树的存储与表示
 顺序存储：将数据结构存储在固定数组中,遍历快但是占空间大,不常见
-链式存储：二叉树是最简单的树,任何树都能转换成二叉树,所以树的存储都是转换成二叉树处理
-        二叉树一般通过链表来实现
+链式存储：二叉树是最简单的树,任何树都能转换成二叉树,所以树的存储都是转换成二叉树处理,二叉树一般通过链表来实现
 
 树的应用场景：
 1.xml、html等,那么编写这些东西的解析器不可避免用到树
@@ -51,8 +50,8 @@ class Tree(object):
         node = Node(item)
         # 先判断树是否为空
         if self.root is None:
+            # 空的话就加上节点
             self.root = node
-            # 新节点已加上,调用方法结束
             return
         # 弄一个队列,把根节点放进去,每次遍历都从根节点往下走
         queue = [self.root]
@@ -62,12 +61,12 @@ class Tree(object):
             cur_node = queue.pop(0)  # 0
             # 判断左子节点是否为空
             if cur_node.lchild is None:
-                # 为空就将新节点node赋值给左子节点
+                # 空的话就加上节点
                 cur_node.lchild = node
-                # 新节点已加上,调用方法结束
                 return
-            # 右子节点同理
+            # 判断右子节点是否为空
             elif cur_node.rchild is None:
+                # 空的话就加上节点
                 cur_node.rchild = node
                 return
             # 都不为空就添加到队列,等待下次循环
@@ -75,11 +74,35 @@ class Tree(object):
                 queue.append(cur_node.lchild)
                 queue.append(cur_node.rchild)
 
-    # 层次遍历(横向遍历)
+    # 1.深度优先遍历：不保留全部结点-->占用空间少,有回溯操作(出入栈)-->运行速度慢
+    # 先序遍历(根-左-右)
+    def pre_order(self, node):
+        if node is None:
+            return
+        print(node.item, end=" ")  # 0 1 3 7 8 4 9 2 5 6
+        self.pre_order(node.lchild)
+        self.pre_order(node.rchild)
+
+    # 中序遍历(左-根-右)
+    def mid_order(self, node):
+        if node is None:
+            return
+        self.mid_order(node.lchild)
+        print(node.item, end=" ")  # 7 3 8 1 9 4 0 5 2 6
+        self.mid_order(node.rchild)
+
+    # 后序遍历(左-右-根)
+    def back_order(self, node):
+        if node is None:
+            return
+        self.back_order(node.lchild)
+        self.back_order(node.rchild)
+        print(node.item, end=" ")  # 7 8 3 9 4 1 5 6 2 0
+
+    # 2.广度优先遍历(层次遍历)：保留全部节点-->占用空间大,没有回溯操作(出入栈)-->运行速度快
     def level_travel(self):
         # 先判断是否为空
         if self.root is None:
-            # 空就直接返回
             return
         # 将根节点放入队列
         queue = [self.root]
@@ -88,7 +111,7 @@ class Tree(object):
             # 每次弹出的都是新队列的头节点
             cur_node = queue.pop(0)
             # 输出当前节点元素
-            print(cur_node.item, end=" ")
+            print(cur_node.item, end=" ")  # 0 1 2 3 4 5 6 7 8 9
             # 判断左子节点
             if cur_node.lchild is not None:
                 # 添加到队列,等待后面循环弹出该节点
@@ -96,30 +119,6 @@ class Tree(object):
             if cur_node.rchild is not None:
                 # 添加到队列,等待后面循环弹出该节点
                 queue.append(cur_node.rchild)
-
-    # 先序遍历(根-左-右)
-    def preorder(self, node):
-        if node is None:
-            return
-        print(node.item, end=" ")
-        self.preorder(node.lchild)
-        self.preorder(node.rchild)
-
-    # 中序遍历(左-根-右)
-    def midorder(self, node):
-        if node is None:
-            return
-        self.midorder(node.lchild)
-        print(node.item, end=" ")
-        self.midorder(node.rchild)
-
-    # 后序遍历(左-右-根)
-    def backorder(self, node):
-        if node is None:
-            return
-        self.backorder(node.lchild)
-        self.backorder(node.rchild)
-        print(node.item, end=" ")
 
 
 if __name__ == "__main__":
@@ -134,12 +133,12 @@ if __name__ == "__main__":
     t.add(7)
     t.add(8)
     t.add(9)
+    t.pre_order(t.root)
+    print("")
+    t.mid_order(t.root)
+    print("")
+    t.back_order(t.root)
+    print("")
     t.level_travel()
-    print("")
-    t.preorder(t.root)
-    print("")
-    t.midorder(t.root)
-    print("")
-    t.backorder(t.root)
 
 
