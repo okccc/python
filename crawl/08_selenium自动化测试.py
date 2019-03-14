84c8e71dc695bcbc2b6967e265d643e456e7f23c
okccc <1573976179@qq.com>
2019-03-14 16:59:56 +0800

    selenium自动化测试

--- crawl/08_selenium自动化测试.py
+++ crawl/08_selenium自动化测试.py
@@ -1,6 +1,12 @@
 # coding=utf-8
 """
 Selenium是一个Web自动化测试工具,会按指定的命令自动操作,可以结合第三方浏览器做网络爬虫
+Selenium很慢：因为requests只会请求当前url,而Selenium会请求当前url+js+css+img所有这些资源,可以设置chrome不加载图片提升速度
+注意事项：
+selenium获取的页面数据是浏览器中elements的内容
+find_element返回一个element,如果没有就会报错,find_elements返回列表,如果没有就是空列表,可根据find_elements结果列表长度判断是否有下一页
+如果页面中包含frame/iframe,需要先调用driver.switch_to_frame方法切换到frame中才能定位元素
+selenium请求第一页数据时会等待页面加载完,而在点击翻页后是不会等待页面加载完的,直接获取数据可能会报错,需要time.sleep(3)
 
 查看网页源代码(sources)：服务器发送到浏览器的原始内容
 查看元素(F12)：经过浏览器渲染,执行js动态生成的内容(如果网站没有使用ajax动态加载其实就等同于源码)
@@ -16,21 +22,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys  # 导入keys调用键盘按键
 import time
+import requests
 
 class Selenium01(object):
     def __init__(self):
         # 设置chrome不加载图片,不然打开页面速度有点慢(可选)
-        options = webdriver.ChromeOptions()
-        prefs = {"profile.managed_default_content_settings.images": 2}
-        options.add_experimental_option("prefs", prefs)
+        # options = webdriver.ChromeOptions()
+        # prefs = {"profile.managed_default_content_settings.images": 2}
+        # options.add_experimental_option("prefs", prefs)
 
         # 创建浏览器对象
-        self.driver = webdriver.Chrome(executable_path="C://chromedriver.exe", chrome_options=options)
+        self.driver = webdriver.Chrome(executable_path="D://chromedriver/chromedriver.exe", )
 
     def introduce(self):
         # 打开百度页面
         self.driver.get("https://www.baidu.com")
-        # 获取input标签(webelement.py模块包含各种定位元素方法)
+        # 截屏
+        self.driver.maximize_window()
+        self.driver.save_screenshot("./screen.png")
+        # 获取input标签 --> webdriver.py模块包含各种定位元素方法
         tag = self.driver.find_element_by_id("kw")
         # 获取标签的某个属性值
         print(tag.get_attribute("class"))  # s_ipt
@@ -38,7 +48,7 @@ class Selenium01(object):
         tag.send_keys("lol")
         # 模拟点击
         self.driver.find_element_by_id("su").click()
-        # 打印网页渲染后的源码
+        # 打印浏览器渲染后的网页elements
         print(type(self.driver.page_source))  # <class 'str'>
         # 获取当前页面cookie
         print(self.driver.get_cookies())
@@ -81,12 +91,12 @@ class Selenium01(object):
         self.driver.quit()
 
     def wait(self):
-        # 打开百度页面
+        # 注意：selenium的显式等待和隐式等待没什么用,建议直接强制等待time.sleep(5)
         self.driver.get("https://www.baidu.com")
         try:
             # 显式等待：设置等待时长并指定条件(常用) ---> 循环页面5秒直到id="xxx"出现
             element = WebDriverWait(self.driver, 5).until(
-                # EC后面接指定条件(expected_conditions.py模块包含各种内置等待条件)
+                # EC后面接指定条件 --> expected_conditions.py模块包含各种内置等待条件
                 EC.presence_of_element_located((By.ID, "abc"))
             )
             print(element)
@@ -134,8 +144,13 @@ class Selenium01(object):
         # 打开百度页面
         self.driver.get("https://www.baidu.com")
         # 获取cookie值
-        for cookie in self.driver.get_cookies():
-            print(cookie)
+        cookies = self.driver.get_cookies()
+        print(cookies)
+        # 字典生成式获取dict格式的cookies
+        cookies = {i["name"]: i["value"] for i in cookies}
+        print(cookies)
+        # 获取网站的cookie之后可以交给requests使用,因为requests速度比selenium快很多
+        requests.get(url="...", cookies=cookies)
         # 删除所有cookie
         self.driver.delete_all_cookies()
         # 退出浏览器
