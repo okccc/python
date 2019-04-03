a8ac009e2267db2d5bd563dc92d8d548d4168930
okccc <1573976179@qq.com>
2019-04-03 18:00:48 +0800

    pandas基本操作

---
+++ analysis/02_pandas基本操作.py
@@ -0,0 +1,120 @@
+"""
+pandas数据结构
+Series：类似一维数组的对象,由数据和索引组成,索引是自动创建的
+DataFrame：类似excel的表格型数据结构,每列数据可以是不同类型,索引包括列索引和行索引
+区别：numpy只能处理数值型数据,pandas可以处理非数值型数据
+"""
+
+import numpy as np
+import pandas as pd
+
+def series():
+    # 1.通过list创建,不指定行索引(默认是自增int类型)
+    ser = pd.Series(range(1, 5))
+    type(ser)  # 对象数据类型
+    print(ser.dtypes)  # 对象中元素数据类型
+    print(ser.index, ser.values, ser[3])  # 查看索引和值,根据索引取值
+    ser.head(), ser.tail(), ser.head(2)  # 默认查看对象的前/后5条数据,也可以指定条数
+
+    # 通过list创建,指定行索引
+    ser = pd.Series(range(10, 15), index=["a", "b", "c", "d", "e"])
+    print(ser.index, ser.values)
+    print(ser[2], ser["c"])  # 索引分标签索引(label)和位置索引(pos)
+    print(ser[1:3], ser["b":"d"])  # 连续索引(切片索引)：标签索引会包含末尾位置
+    print(ser[[0, 2]], ser[["a", "c"]])  # 不连续索引
+
+    # series合并
+    s1 = pd.Series(range(10, 15))
+    s2 = pd.Series(range(20, 23))
+    s1.add(s2)  # 两个Series对象合并,缺失值以NAN代替
+    s1.add(s2, fill_value=0)  # 两个Series对象合并,先将缺失值以0填充再参与运算
+
+    # 2.通过dict创建(key是行索引)
+    pd.Series({"a": 11.1, "b": 22.2, "c": 33.3})
+
+def dataframe():
+    # 1.通过numpy创建,不指定行/列索引,默认是自增int类型
+    df = pd.DataFrame(np.random.rand(3, 4))
+    type(df)  # 对象数据类型
+    print(df.dtypes)  # 对象中元素数据类型
+    print(df.index, df.columns, df.values, df[2])  # 对象的行/列索引和值,默认按列索引取值
+    df.info()  # 展示当前df在内存中的信息
+    df.head(2)  # 指定行数取值
+
+    # 通过numpy创建,指定行/列索引
+    df = pd.DataFrame(np.random.rand(3, 4), index=["A", "B", "C"], columns=["a", "b", "c", "d"])
+    print(df["c"], df["c"].values)
+    # 连续索引(切片索引)：dataframe不好直接切片,需借助loc和iloc且必须先指定行索引
+    print(df.loc["A": "B", "c": "d"])
+    print(df.iloc[0:2, 2:4])
+    print(df[["a", "c"]])  # 不连续索引
+
+    # dataframe合并
+    df1 = pd.DataFrame(np.random.rand(2, 3))
+    df2 = pd.DataFrame(np.random.rand(3, 4))
+    df1.add(df2)  # 两个DataFrame对象合并,缺失值以NAN代替
+    df1.add(df2, fill_value=0)  # 两个DataFrame对象合并,先将缺失值以0填充再参与运算
+
+    # 2.通过dict创建(key是列索引,行索引默认是自增int类型)
+    df = pd.DataFrame({
+            "A": 1.0,  # float
+            "B": pd.Timestamp("20170625"),  # timestamp
+            "C": pd.Series(range(10, 14)),  # Series
+            "D": ["python", "C", "C++", "Java"],  # list
+            "E": np.array([10] * 4),  # ndarray
+            "F": "orc"  # str
+        })
+    print(df["D"])  # 查找指定列
+    print(df["D"][3])  # 查找指定列的指定行
+    df["G"] = df["C"] * 2  # 添加列
+    del(df["F"])  # 删除列
+
+def func():
+    # describe()
+    ser = pd.Series(range(10, 15))
+    ser.sum()
+    ser.describe()  # 统计描述
+    df = pd.DataFrame(np.random.rand(3, 4))
+    df.sum()  # 默认axis=0按列计算
+    df.sum(axis=1, skipna=False)  # 可以指定axis=1按行计算,skipna是否排除缺失值
+    df.describe()
+
+    # df.apply(func): 作用于指定的行/列,func可以是内置函数也可以是自定义函数
+    df.max(), df.apply(lambda x: x.max())  # 默认按列计算
+    df.max(axis=1), df.apply(lambda x: x.max(), axis=1)  # 按行计算
+    # df.applymap(func): 作用于每一个元素
+    df.applymap(lambda x: "%.2f" % x)  # 将每个元素保留两位小数
+
+    # 排序
+    ser = pd.Series(range(10, 15), index=[np.random.randint(low=1, high=20, size=(5,))])
+    ser.sort_index(), ser.sort_index(ascending=False)  # 按索引排序(默认升序)
+    ser.sort_values(), ser.sort_values(ascending=False)  # 按值排序(默认升序)
+    df = pd.DataFrame(np.random.rand(3, 4), index=["A", "C", "B"], columns=["b", "d", "a", "c"])
+    df.sort_index()   # 默认axis=0按行索引升序排序(此处axis特殊)
+    df.sort_index(axis=1, ascending=False)
+    df.sort_values(by="c")  # 按值排序：by=列名,默认升序
+    df.sort_values(by="c", ascending=False)
+
+    # nan值
+    df = pd.DataFrame([np.random.randn(4), [10., np.nan, 20., np.nan], [30., np.nan, np.nan, 40.]])
+    df.isna()  # 判断是否是缺失值
+    df.dropna()  # 丢弃缺失数据的行/列(默认axis=0按行处理,how="any"只要有nan值就删除所在行/列)
+    df.dropna(axis=1, how="all")  # 指定how="all"只有全部为nan时才删除该行/列
+    df.fillna(50)  # 填充缺失值
+
+    # 多层索引
+    ser = pd.Series(range(10, 15), index=[["a", "a", "b", "c", "c"], [10, 20, 30, 10, 20]])
+    type(ser.index)  # 查看索引类型
+    print(ser.index)  # 查看索引
+    print(ser["b"])  # 根据外层索引取值
+    print(ser["a", 10])  # 取出外层索引为a内层索引为10的值
+    print(ser[:, 20])  # 取出所有外层索引其内层索引为20的值
+    ser.swaplevel()  # 交换分层索引：0最外层、1次外层,只有两层就不用写参数,就是最外层与次外层交换
+    ser.sort_index()  # 按层索引排序：默认level=0最外层、level=1次外层。。。
+    ser.sort_index(level=1)
+    ser.swaplevel().sort_index()  # 先交换分层再按索引排序
+
+    # 重构
+    ser.unstack()  # 默认level=-1将最内层的索引变成列索引,匹配不到的就给NaN值
+    ser.unstack(level=0)  # level=0可以将外层索引变成列索引
+    ser.unstack().stack()
