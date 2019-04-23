c453295b727d55d37208dda29528fe22b1c2a629
okccc <1573976179@qq.com>
2019-04-23 17:31:40 +0800

    逻辑回归

--- ml/03_回归算法.py
+++ ml/03_回归算法.py
@@ -1,19 +1,22 @@
 # coding=utf-8
 import numpy as np
+import pandas as pd
 # 数据集
 from sklearn.datasets import load_boston  # 数据案例
 from sklearn.model_selection import train_test_split  # 数据集分割
 # 特征工程
 from sklearn.preprocessing import StandardScaler  # 特征预处理之标准化缩放
 # 回归算法
-from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge  # 正规方程, 梯度下降, 领回归
-# 回归性能评估
-from sklearn.metrics import mean_squared_error  # 均方误差
+from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge, LogisticRegression  # 正规方程, 梯度下降, 岭回归, 逻辑回归
+# 模型性能评估
+from sklearn.metrics import mean_squared_error, classification_report  # 均方误差, 分类报告
+# 模型保存和加载
+from sklearn.externals import joblib
 
 
 def linear():
     """
-    线性回归预测房子价格
+    线性回归预测房价
     """
 
     # 1.原始数据
@@ -32,12 +35,21 @@ def linear():
     std_y.fit_transform(np.reshape(y_train, newshape=(-1, 1)))
     std_y.transform(np.reshape(y_test, newshape=(-1, 1)))
 
+    # 加载之前保存的模型进行预测
+    # model = joblib.load("./lr.pkl")
+    # y_predict = std_y.inverse_transform(model.predict(x_test))
+    # print("保存的模型的预测结果：%s" % y_predict)
+
     # 3.算法预测和模型评估
     # 1).正规方程
     lr = LinearRegression()
     lr.fit(x_train, y_train)
     # 输出权重参数
     print(lr.coef_)
+
+    # 保存训练好的模型,下次就可以不用训练直接使用该模型
+    joblib.dump(lr, "./lr.pkl")
+
     # 预测测试集的房子价格
     y_lr_predict = std_y.inverse_transform(lr.predict(x_test))
     print("正规方程的预测结果：%s" % y_lr_predict)
@@ -58,10 +70,45 @@ def linear():
     print(r.coef_)
     # 预测测试集的房子价格
     y_r_predict = std_y.inverse_transform(r.predict(x_test))
-    print("梯度下降的预测结果：%s" % y_r_predict)
-    print("梯度下降的均方误差：%s" % mean_squared_error(std_y.inverse_transform(y_test), y_r_predict))
+    print("岭回归的预测结果：%s" % y_r_predict)
+    print("岭回归的均方误差：%s" % mean_squared_error(std_y.inverse_transform(y_test), y_r_predict))
 
+
+def logistic():
+    """
+    逻辑回归做二分类癌症预测
+    """
+    # 1.原始数据
+    columns = ['Sample code number', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape',
+              'Marginal Adhesion', 'Single Epithelial Cell Size', 'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli',
+              'Mitoses', 'Class']
+    df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data", names=columns)
+    print(df)
+
+    # 2.pandas处理数据
+    # 报错：ValueError: could not convert string to float: '?'
+    # 先根据df.dtypes确定是哪一列的数据会有?值,然后删除包含?的行
+    df = df[df["Bare Nuclei"].apply(lambda x: x != "?")]
+    df.dropna()
+
+    # 3.特征工程
+    # 数据集分割
+    x_train, x_test, y_train, y_test = train_test_split(df[columns[1:10]], df[columns[10]], test_size=0.25)
+    # 对特征值做标准化处理
+    std = StandardScaler()
+    std.fit_transform(x_train)
+    std.transform(x_test)
+
+    # 4.算法预测和模型评估
+    lr = LogisticRegression(penalty="l2", C=1.0)
+    lr.fit(x_train, y_train)
+    y_predict = lr.predict(x_test)
+    print("预测结果：%s" % y_predict)
+    score = lr.score(x_test, y_test)
+    print("准确率：%s" % score)
+    report = classification_report(y_test, y_predict, labels=[2, 4], target_names=["良性", "恶性"])
+    print("召回率：%s" % report)
 
 
 if __name__ == '__main__':
-    linear()
+    logistic()
