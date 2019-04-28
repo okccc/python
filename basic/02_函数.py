db13316e37172e38ba27021256b9e7562768a823
okccc <1573976179@qq.com>
2019-04-28 15:37:30 +0800

    print(f'')格式化输出相当于print(''.format())

--- basic/02_函数.py
+++ basic/02_函数.py
@@ -18,14 +18,12 @@ def test01(name, gender=True):
         gender_value = "女生"
     print("%s是%s" % (name, gender_value))
 
-
-def cumulate(n):
+def cumulate01(n):
     # 累加案例
     if n == 1:
         return 1
     else:
-        return cumulate(n - 1) + n
-
+        return cumulate01(n - 1) + n
 
 def fibonacci(n):
     # 菲波那切数列
@@ -34,7 +32,6 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-
 def input01():
     # input(): 键盘录入只能输入字符串,数字类型要用int,float等函数转换
     price = float(input("请输入单价:"))
@@ -42,12 +39,10 @@ def input01():
     money = price * weight
     print(money)
 
-
 def eval01():
     # eval(): 可以将input输入的字符串当成有效表达式(取字符串里的内容)并返回计算结果
     result = eval(input("输入算术题:"))
     print(result)
-
 
 def id01():
     # id(): 可以查看变量在内存中的地址值
@@ -55,32 +50,21 @@ def id01():
     print("%d" % id(num))  # 1537823824 (%d以十进制输出结果)
     print("%x" % id(num))  # 5ba95450 (%x以十六进制输出结果)
 
-
 def print01():
     """
     print(): 默认换行,在结尾添加end=""可以不换行接着输出
-    %表示格式化输出符: 当print函数输出的内容包含多种不同类型的变量,就要对变量进行格式化
+    %表示格式化输出符: 当print输出内容包不同数据类型的变量时要对变量格式化
     %s:字符串  %d:整数  %f:小数  %%:输出%
+    print(f'')表示格式化,相当于print(''.format())
     """
-
     # 格式化输出字符串
-    name = "小花"
-    print("大家好我叫 %s" % name)  # 大家好我叫 小花
-
+    print("大家好我叫 %s" % "小花")  # 大家好我叫 小花
     # 格式化输出整数(06控制长度,不满6位以0补全)
-    num = 19
-    print("我的学号是 %06d" % num)  # 我的学号是 000019
-
+    print("我的学号是 %06d" % 19)  # 我的学号是 000019
     # 格式化输出小数(.2控制长度,小数点后面保留2位)
-    price = 4.5
-    weight = 5
-    money = price * weight
-    print("苹果单价 %.2f 元/斤,重量 %d 斤,总价 %.2f 元" % (price, weight, money))  # 多个变量放()用逗号隔开
-
+    print("苹果单价 %.2f 元/斤,重量 %d 斤,总价 %.2f 元" % (4.5, 5, 4.5*5))  # 多个变量放()用逗号隔开
     # 格式化输出百分比
-    scale = 0.25
-    print("数据比例是 %.2f%%" % (scale * 100))
-
+    print("数据比例是 %.2f%%" % (0.25 * 100))
 
 def high_order():
     """
@@ -107,9 +91,3 @@ def high_order():
     print(res6)  # [2, 4]
 
 
-if __name__ == '__main__':
-    # input01()
-    # eval01()
-    # id01()
-    # print01()
-    high_order()
