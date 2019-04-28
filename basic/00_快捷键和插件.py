e3f966b9478123409099a7b73f72b132822ff330
okccc <1573976179@qq.com>
2019-04-28 14:31:57 +0800

    pycharm安装Mongo Plugin插件

--- basic/00_快捷键和插件.py
+++ basic/00_快捷键和插件.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """
-PyCharm快捷键大全
+Pycharm快捷键
 代码提示部分：m方法(self);f函数;v变量;c类;p参数
 Ctrl + Alt + S：设置
 Ctrl + Alt + L：代码格式化
@@ -9,4 +9,8 @@ F2：查看函数详细注释
 Ctrl + F：快速查找
 Ctrl + Home：跳到顶部
 Ctrl + End：跳到底部
+
+Pycharm插件
+Statistic：当前项目的统计信息
+Mongo Plugin：mongodb的可视化数据库工具  View --> Tool Windows --> Mongo Explorer
 """
