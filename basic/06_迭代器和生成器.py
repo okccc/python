ae249cc7770cbe66d1a68cbdb6e7b85ce46c4adb
okccc <1573976179@qq.com>
2019-05-10 11:33:23 +0800

    迭代器和可迭代对象

--- basic/06_迭代器和生成器.py
+++ basic/06_迭代器和生成器.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 """
-可迭代对象(Iterable)：可以作用于for循环的对象,包含iter()方法,可用isinstance(**, Iterable)判断
-迭代器(Iterator)：有返回值的可迭代对象,包含iter()和next()方法,可用isinstance(**, Iterator)判断
-iter()函数：'',(),[]等都是Iterable,但不是Iterator,iter()可以将Iterable变成Iterator,生成器一定是迭代器
-生成器(generator)：使用yield关键字的函数不再是普通函数,调用该函数不会立即执行而是创建一个生成器对象,调用next()时运行
+可迭代对象：可以作用于for循环的对象,包含iter()方法,可用isinstance(**, Iterable)判断
+迭代器：有返回值的可迭代对象,包含iter()和next()方法,可用isinstance(**, Iterator)判断
+iter()：迭代器一定是可迭代对象但可迭代对象不一定是迭代器,iter函数可以将可迭代对象变成迭代器
+生成器：使用yield关键字的函数不再是普通函数,调用该函数不会立即执行而是创建一个生成器对象,调用next()时运行
 yield作用：中断函数并返回后面的值,然后在下一次执行next()方法时从当前位置继续运行,减少内存占用
-优点：列表是先生成再调用,迭代器和生成器不保存数据而是保存数据的生成方式,调用时才生成,极大地节省内存空间
+优点：列表和字典等容器是先生成再调用,迭代器和生成器不保存数据而是保存数据的生成方式,调用时才生成极大节省内存空间
 """
 
 from collections import Iterable
@@ -27,14 +27,15 @@ def iterator():
     print(isinstance((), Iterator))  # False
     print(isinstance([], Iterator))  # False
     print(isinstance({}, Iterator))  # False
-    print(isinstance((i for i in range(1, 10)), Iterator))  # True
+    print(isinstance((i for i in range(1, 10)), Iterator))  # True(生成器一定是迭代器)
     # iter()方法将可迭代对象变成迭代器通过next()不断调用取值,迭代器就是有返回值的可迭代对象
-    L = [11, 22, 33]
-    I = iter(L)
-    print(I)  # <list_iterator object at 0x0000020B96AFD198>
-    print(next(I))  # 11
-    print(next(I))  # 22
-    print(next(I))  # 33
+    a = [11, 22, 33]
+    print(isinstance(a, Iterator))  # False
+    b = iter(a)
+    print(isinstance(b, Iterator))  # True
+    print(next(b))  # 11
+    print(next(b))  # 22
+    print(next(b))  # 33
 
 
 def fibonacci(n):
