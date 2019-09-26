817270f4ce529fdba1fc697107bf77b008344455
okccc <1573976179@qq.com>
2019-09-26 16:43:50 +0800

    hive拉链表

--- hadoop/shell/mysql2hive.sh
+++ hadoop/shell/mysql2hive.sh
@@ -15,8 +15,8 @@ column=$5
 username=eadmin
 password='EWQTB512Oikf;'  # 密码字符串最好加''防止有$等特殊字符导致access denied
 
-log_path=/home/hive/gds/logs/sqoop/input/$YESTERDAY
-watch_path=/home/hive/gds/filewatch/sqoop/input/$YESTERDAY
+log_path=/home/hive/gds/logs/sqoop/input/${YESTERDAY}
+watch_path=/home/hive/gds/filewatch/sqoop/input/${YESTERDAY}
 
 if [[ ! -d ${log_path} ]] ; then
     mkdir ${log_path}
@@ -36,7 +36,7 @@ sqoop import --connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=fals
 
 # 增量抽数据
 sqoop import --connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=false
---username ${username} --password ${password} --table ${table} --fields-terminated-by '\001' --where "create_time >=${YESTERDAY} and create_time<${TODAY}"
+--username ${username} --password ${password} --table ${table} --fields-terminated-by '\001' --where "create_time=${YESTERDAY} or update_time=${YESTERDAY}"
 --hive-drop-import-delims --hive-import --hive-table base.${table} -m 4 --delete-target-dir --hive-overwrite --hive-partition-key dt --hive-partition-value ${YESTERDAY}
 --null-string '\\N' --null-non-string '\\N' &>> ${log_path}/${table}.log
 
@@ -52,20 +52,20 @@ fi
 
 # 参数说明：
 sqoop import
---connect jdbc:mysql://$ip:$port/$mysql_db?tinyInt1isBit=false
---username $username
---password $password
---table $table
---fields-terminated-by '\001'                               			    //设置分隔符
---where "$column >=$YESTERDAY and $column<$TODAY"                           //筛选数据
---hive-drop-import-delims                                   			    //过滤掉分隔符\n, \r, \01
---hive-import                                               	            //导入hive,不设置就使用hive默认分隔符
---hive-table base.$table -m 4                                               //-m表示maptask数量
---delete-target-dir                                                         //删除已存在的目录
---hive-overwrite                                                            //覆盖hive表中已经存在的数据
---hive-partition-key dt                                                     //分区表分区字段dt
---hive-partition-value $YESTERDAY
---null-string '\\N'                                        			        //将字符串空值写成\\N
---null-non-string '\\N' &>> $log_path/$table.log                            //将非字符串空值写成\\N
+--connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=false
+--username ${username}
+--password ${password}
+--table ${table}
+--fields-terminated-by '\001'                               			    # 设置分隔符
+--where "create_time=${YESTERDAY} or update_time=${YESTERDAY}"              # 筛选数据
+--hive-drop-import-delims                                   			    # 过滤掉分隔符\n, \r, \01
+--hive-import                                               	            # 导入hive,不设置就使用hive默认分隔符
+--hive-table base.${table} -m 4                                               # -m表示maptask数量
+--delete-target-dir                                                         # 删除已存在的目录
+--hive-overwrite                                                            # 覆盖数据
+--hive-partition-key dt                                                     # 分区字段dt
+--hive-partition-value ${YESTERDAY}                                         # 分区值
+--null-string '\\N'                                        			        # 将字符串空值写成\\N
+--null-non-string '\\N' &>> ${log_path}/${table}.log                        # 将非字符串空值写成\\N
 
 
