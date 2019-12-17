f70397fd35fd8286e21a3ec4c21f5aeb37b1ca96
okccc <1573976179@qq.com>
2019-12-17 15:57:25 +0800

    表和视图区别

--- db/01_mysql基础.sql
+++ db/01_mysql基础.sql
@@ -118,7 +118,7 @@ show profiles;
 show index from person;
 -- 删除索引
 alter table person drop index pindex;
--- 创建视图(将复杂的查询sql封装成虚拟表)
+-- 创建视图(将复杂的查询sql封装成虚拟表,表可读可写视图只能读不能写)
 create view view_name as select id,name,age from person where sex='男';
 -- 查看视图
 select * from view_name;
